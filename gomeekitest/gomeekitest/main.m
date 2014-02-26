4d52a36748d5aa5f6c3d601862e7e00587895995
suphaporn <suphaporn.louiz@gmail.com>
2014-02-26 20:59:33 +0700

    Test

---
+++ gomeekitest/gomeekitest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gomeekitest
+//
+//  Created by louiz on 2/26/2557 BE.
+//  Copyright (c) 2557 louiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "louizAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([louizAppDelegate class]));
+    }
+}
